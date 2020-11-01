2b959f322868c662d99e81cdff1569cf3da5b6cc
Coal33 <takasum88@outlook.jp>
2020-11-02 01:45:07 +0900

    開発開始

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,91 +1,125 @@
-﻿
-//
-// OpenSiv3D v0.4.3 ゲームテンプレート
-//
-
-//
-// macOS では README.md と同じフォルダに
-// include と lib をコピーしてください。
-//
-
-#include "Common.hpp"
-#include "Game.hpp"
-#include "Title.hpp"
+#include <Siv3D.hpp>
 
 void Main() {
-	// （Esc キーで終了しないようにする場合はコメントを外す）
-	// System::SetTerminationTriggers(UserAction::CloseButtonClicked);
+    Window::Resize(640, 480);
+    constexpr ColorF background(0.6, 0.8, 0.7);
+    Scene::SetBackground(background);
 
-	// タイトルを設定
-	Window::SetTitle(U"ブロックくずし");
+    constexpr Ellipse buttonLB(160, 140, 50, 24);
+    constexpr Ellipse buttonRB(520, 140, 50, 24);
 
-	// 背景色を設定
-	Scene::SetBackground(ColorF(0.2, 0.8, 0.4));
+    constexpr RectF leftTrigger(150, 16, 40, 100);
+    constexpr RectF rightTrigger(500, 16, 40, 100);
 
-	// 使用するフォントアセットを登録
-	FontAsset::Register(
-	    U"Title", 120, U"example/font/AnnyantRoman/AnnyantRoman.ttf");
-	FontAsset::Register(U"Menu", 30, Typeface::Regular);
-	FontAsset::Register(U"Score", 36, Typeface::Bold);
+    constexpr Circle buttonLThumb(170, 250, 35);
+    constexpr Circle buttonRThumb(420, 350, 35);
+    constexpr Circle buttonDPad(260, 350, 40);
 
-	// シーンと遷移時の色を設定
-	MyApp manager;
-	manager.add<Title>(State::Title)
-	    .add<Game>(State::Game)
-	    .setFadeColor(ColorF(1.0));
+    constexpr Circle buttonA(510, 300, 20);
+    constexpr Circle buttonB(560, 250, 20);
+    constexpr Circle buttonX(460, 250, 20);
+    constexpr Circle buttonY(510, 200, 20);
+    constexpr Circle buttonBack(270, 250, 15);
+    constexpr Circle buttonStart(410, 250, 15);
 
-	// （ゲームシーンから開始する場合はコメントを外す）
-	// manager.init(State::Game);
+    // ユーザインデックス (0 - 3)
+    size_t userIndex = 0;
 
-	while(System::Update()) {
-		if(!manager.update()) {
-			break;
-		}
-	}
+    // デッドゾーンの有効無効
+    bool deadZone = false;
+
+    // 振動 (0.0 - 1.0)
+    double leftV = 0.0, rightV = 0.0;
+
+    while(System::Update()) {
+        // 指定したユーザインデックスの XInput コントローラを取得
+        auto controller = XInput(userIndex);
+
+        if(deadZone) {
+            controller.setLeftTriggerDeadZone();
+            controller.setRightTriggerDeadZone();
+            controller.setLeftThumbDeadZone();
+            controller.setRightThumbDeadZone();
+        } else {
+            controller.setLeftTriggerDeadZone(DeadZone::Disabled());
+            controller.setRightTriggerDeadZone(DeadZone::Disabled());
+            controller.setLeftThumbDeadZone(DeadZone::Disabled());
+            controller.setRightThumbDeadZone(DeadZone::Disabled());
+        }
+
+        // 振動
+        controller.setVibration(leftV, rightV);
+
+        buttonLB.draw(ColorF(controller.buttonLB.pressed() ? 1.0 : 0.7));
+        buttonRB.draw(ColorF(controller.buttonRB.pressed() ? 1.0 : 0.7));
+
+        Ellipse(340 + 3.0 * Random(leftV + rightV), 480, 300, 440)
+            .draw(ColorF(0.9));
+        Ellipse(340, 40, 220, 120).draw(background);
+        Circle(340, 660, 240).draw(background);
+
+        Circle(340, 250, 30).draw(ColorF(0.6));
+
+        // コントローラの接続の有無
+        if(controller.isConnected()) {
+            Circle(340, 250, 32)
+                .drawPie(-0.5_pi + 0.5_pi * controller.userIndex, 0.5_pi,
+                         ColorF(0.7, 1.0, 0.5));
+        }
+
+        Circle(340, 250, 25).draw(ColorF(0.6));
+
+        leftTrigger.draw(AlphaF(0.25));
+        leftTrigger
+            .stretched((controller.leftTrigger - 1.0) * leftTrigger.h, 0, 0, 0)
+            .draw();
+
+        rightTrigger.draw(AlphaF(0.25));
+        rightTrigger
+            .stretched((controller.rightTrigger - 1.0) * rightTrigger.h, 0, 0,
+                       0)
+            .draw();
+
+        buttonLThumb.draw(
+            ColorF(controller.buttonLThumb.pressed() ? 0.85 : 0.5));
+        Circle(buttonLThumb.center +
+                   Vec2(controller.leftThumbX, -controller.leftThumbY) * 25,
+               20)
+            .draw();
+
+        buttonRThumb.draw(
+            ColorF(controller.buttonRThumb.pressed() ? 0.85 : 0.5));
+        Circle(buttonRThumb.center +
+                   Vec2(controller.rightThumbX, -controller.rightThumbY) * 25,
+               20)
+            .draw();
+
+        buttonDPad.draw(ColorF(0.5));
+
+        const Vec2 direction(
+            controller.buttonRight.pressed() - controller.buttonLeft.pressed(),
+            controller.buttonDown.pressed() - controller.buttonUp.pressed());
+
+        if(!direction.isZero()) {
+            Circle(buttonDPad.center + direction.normalized() * 25, 15).draw();
+        }
+
+        buttonA.draw(ColorF(0.0, 1.0, 0.3)
+                         .setA(controller.buttonA.pressed() ? 1.0 : 0.3));
+        buttonB.draw(ColorF(1.0, 0.0, 0.3)
+                         .setA(controller.buttonB.pressed() ? 1.0 : 0.3));
+        buttonX.draw(ColorF(0.0, 0.3, 1.0)
+                         .setA(controller.buttonX.pressed() ? 1.0 : 0.3));
+        buttonY.draw(ColorF(1.0, 0.5, 0.0)
+                         .setA(controller.buttonY.pressed() ? 1.0 : 0.3));
+
+        buttonBack.draw(ColorF(controller.buttonBack.pressed() ? 1.0 : 0.7));
+        buttonStart.draw(ColorF(controller.buttonStart.pressed() ? 1.0 : 0.7));
+
+        SimpleGUI::RadioButtons(userIndex, {U"1P", U"2P", U"3P", U"4P"},
+                                Vec2(10, 10));
+        SimpleGUI::CheckBox(deadZone, U"DeadZone", Vec2(240, 10));
+        SimpleGUI::Slider(U"left", leftV, Vec2(240, 50));
+        SimpleGUI::Slider(U"right", rightV, Vec2(240, 90));
+    }
 }
-
-//
-// = クロスプラットフォーム開発の注意 =
-//
-// Xcode でソースコードを保存すると UTF-8 形式 (BOM 無し) になります。
-// MSVC で作業するときはソースコードを UTF-8 (BOM 付き) に直す必要があります。
-//
-//
-// = アドバイス =
-// macOS 10.15 Catalina の Xcode プロジェクトで、
-// ビルドしたプログラムを起動するたびにファイルアクセス許可のダイアログが表示される場合、
-// プロジェクトのフォルダを User/アプリケーション
-// に移動させることで通常は表示されなくなります。
-// 特別なファイルシステム関数の使用や、Web
-// カメラ、マイク使用時のダイアログまでは消せません。
-//
-//
-// = アドバイス =
-// Debug ビルドではプログラムの最適化がオフになります。
-// 実行速度が遅いと感じた場合は Release ビルドを試しましょう。
-// アプリをリリースするときにも、Release ビルドにするのを忘れないように！
-//
-// 思ったように動作しない場合は「デバッグの開始」でプログラムを実行すると、
-// 出力ウィンドウに詳細なログが表示されるので、エラーの原因を見つけやすくなります。
-//
-//
-// = お役立ちリンク =
-//
-// OpenSiv3D リファレンス
-// https://siv3d.github.io/ja-jp/
-//
-// チュートリアル
-// https://siv3d.github.io/ja-jp/tutorial/basic/
-//
-// よくある間違い
-// https://siv3d.github.io/ja-jp/articles/mistakes/
-//
-// サポートについて
-// https://siv3d.github.io/ja-jp/support/support/
-//
-// Siv3D ユーザコミュニティ Slack への参加
-// https://siv3d.github.io/ja-jp/community/community/
-//
-// 新機能の提案やバグの報告
-// https://github.com/Siv3D/OpenSiv3D/issues
-//
