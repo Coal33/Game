b38e474736f8b331dd9be600f6d76235b42c3036
Coal33 <takasum88@outlook.jp>
2020-11-08 00:59:28 +0900

    Fix: Defineのstatic変数にconstつけて定数化した

--- src/Define.hpp
+++ src/Define.hpp
@@ -11,10 +11,10 @@ using ll       = long long;
 using SceneMgr = SceneManager<SceneState>;
 
 
-static String gameName     = U"GameName";  //ゲームの名前
-static String gameVersion  = U"v0.0.1";    //ゲームのバージョン名
-static int m_buildNumber   = 1;            //ビルド番号
-static String tweetMessage = U"";          //ツイートメッセージ
+static const String gameName     = U"GameName";  //ゲームの名前
+static const String gameVersion  = U"v0.0.1";    //ゲームのバージョン名
+static const int m_buildNumber   = 1;            //ビルド番号
+static const String tweetMessage = U"";          //ツイートメッセージ
 
 
 struct AbilityValues {
